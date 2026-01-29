7c7b572a34e117d829e6efa869be09f6255820ca
Latha-107 <latha_paladugu@trigent.com>
2026-01-29 15:23:45 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Debug middleware to log incoming paths
+app.use((req, res, next) => {
+    console.log(`Request path: ${req.path}`);
+    next();
+});
+
 // Root route
 app.get('/', (req, res) => {
     res.send('Application is running on root (/)');
@@ -12,7 +18,7 @@ app.get('/testing-load', (req, res) => {
     res.send('Testing Load route is working!');
 });
 
-// Optional catch-all for undefined routes
+// Catch-all for undefined routes
 app.get('*', (req, res) => {
     res.status(404).send(`Cannot GET ${req.path}`);
 });
